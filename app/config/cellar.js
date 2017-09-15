e7538ada1e30b8b3627c225a144375017d51e891
Christopher Rackley <masterprocess@The-Beluga.local>
2017-09-15 12:56:17 -0400

    added more to cellar model but it still crashes the server

--- app/config/cellar.js
+++ app/config/cellar.js
@@ -1,11 +1,57 @@
-module.exports = (sequelize, DataTypes) => {
-    var Wine = sequelize.define('newWine', {
-        WineName: {
-            type: DataTypes.STRING
-        },
-        WineDescription: {
-            type: DataTypes.TEXT
-        }
-    })
+    module.exports = (sequelize, DataTypes) => {
+        var User = sequelize.define('Wine', {
+            wineName: {
+                type: DataTypes.STRING
+                },
+            wineCode: {
+                type: DataTypes.TEXT,
+                allowNull:false
+            },
+            wineLink: {
+                type: DataTypes.TEXT,
+                allowNull: true
+            },
+            winePrice: {
+                type: DataTypes.INTEGER,
+                allowNull: true
+            },
+            wineRegion: {
+                type: DataTypes.TEXT,
+                allowNull: true
+            },
+            wineRank: {
+                type: DataTypes.TEXT,
+                allowNull: true
+            },
+            wineType: {
+                type: DataTypes.TEXT,
+                allowNull: true
+            },
+            wineVarietal: {
+                type: DataTypes.TEXT,
+                allowNull: true
+            },
+            wineVintage: {
+                type: DataTypes.TEXT,
+                allowNull: true
+            },
+            wineWinery: {
+                type: DataTypes.TEXT,
+                allowNull: true
+            },
+            wineWineryID: {
+                type: DataTypes.TEXT,
+                allowNull: true
+            },
 
-    };
+               
+        })
+    
+       // User.associate = (models) => {
+            
+          //  User.hasMany(models.cellar, {
+           //     onDelete: "CASCADE"
+           // })      
+       // }
+
+    }
